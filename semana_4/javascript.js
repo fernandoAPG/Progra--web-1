bf88ecc75bba98dd8dd533cd1a719f28299182f5
Fernando Peñaloza <106409015+fernandoAPG@users.noreply.github.com>
2025-04-21 21:48:10 -0500

    añadido javascript

---
+++ semana_4/javascript.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo");
+let numero = 10; //number
+let nombre = "Ignacio"; //string
+let esMayorEdad = true; //boolean
+let edad = null;
+let peso = undefined;
+
+//Colecciones
+let listanumeros = [1, 2, 3, 4, 5];
+let lista = [1, "pepito"];
+let alumno = {
+  nombre: "Ignacio",
+  edad: 24,
+  hobbies: ["Leer", "tocar guitarra"],
+  matriculado: true,
+  universidad: {
+    nombre: "Universidad de Lima",
+    direccion: "AV jP",
+  },
+};
